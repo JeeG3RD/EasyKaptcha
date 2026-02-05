01678df979a578cbb41090b703a02a3e1e4f84e4
JeeG3RD <gerdy.jerome@gmail.com>
2026-02-05 11:56:17 +0100

    Started Class initialisation process

---
+++ dist/js/core.js
@@ -0,0 +1,25 @@
+class EasyKaptcha {
+  constructor(){}
+
+  captcha(config) {
+    // We search for the form by ID in the document
+    const form = document.getElementById(
+      config === null || config === void 0 ? void 0 : config.formId,
+    );
+
+    if (!form) {
+      console.error("EasyKaptcha ERROR : Form not found.");
+      return;
+    }
+
+    // Then we search the confirm button, also by ID
+    const confirmButton = document.getElementById(
+      config === null || config === void 0 ? void 0 : config.buttonId,
+    );
+
+    if (!confirmButton) {
+      console.error("EasyKaptcha ERROR : Confirm button not found.");
+      error;
+    }
+  }
+}
