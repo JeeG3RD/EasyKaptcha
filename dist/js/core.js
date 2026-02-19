cc1b278978c269497813c2d636208715f57550a3
JeeG3RD <gerdy.jerome@gmail.com>
2026-02-19 19:11:55 +0100

    Reworked init method

--- dist/js/core.js
+++ dist/js/core.js
@@ -1,29 +1,49 @@
 class EasyKaptcha {
+  form = null;
+  button = null;
+  zone = null;
+
+  //
   constructor(){}
 
   init(config) {
+    if (false === this.checkConfig(config))
+      return;
+    
+    // Continue the init
+  }
+
+  checkConfig(config) {
     // We search for the form by ID in the document
-    const form = document.getElementById(
+    this.form = document.getElementById(
       config === null || config === void 0 ? void 0 : config.formId,
     );
 
-    if (!form) {
+    if (!this.form) {
       console.error("EasyKaptcha ERROR : Form not found.");
-      return;
+      return false;
     }
 
     // Then we search the confirm button, also by ID
-    const confirmButton = document.getElementById(
+    this.button = document.getElementById(
       config === null || config === void 0 ? void 0 : config.buttonId,
     );
 
-    if (!confirmButton) {
+    if (!this.button) {
       console.error("EasyKaptcha ERROR : Confirm button not found.");
-      error;
+      return false;
     }
 
-    
+    // Finally we search for the zone where captcha will be shown
+    this.zone = document.getElementById(
+      config === null || config === void 0 ? void 0 : config.zoneId,
+    )
+
+    if (!this.zone) {
+      console.error("EasyKaptcha ERROR : Zone not found.");
+      return false;
+    }
+
+    return true
   }
 }
-
-export default EasyKaptcha;
